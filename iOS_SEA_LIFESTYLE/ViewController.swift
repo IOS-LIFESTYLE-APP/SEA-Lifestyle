9974a41d3aac24e45feb3176e7663ca62549d91b
Ayo <ayomide.ajayi2839@gmail.com>
2020-04-09 01:06:25 -0400

    Storyboard files

---
+++ iOS_SEA_LIFESTYLE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_SEA_LIFESTYLE
+//
+//  Created by Ayo  on 4/9/20.
+//  Copyright © 2020 Ayo . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS_SEA_LIFESTYLETests/iOS_SEA_LIFESTYLETests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_SEA_LIFESTYLETests.swift
+//  iOS_SEA_LIFESTYLETests
+//
+//  Created by Ayo  on 4/9/20.
+//  Copyright © 2020 Ayo . All rights reserved.
+//
+
+import XCTest
+@testable import iOS_SEA_LIFESTYLE
+
+class iOS_SEA_LIFESTYLETests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
